30df75a1091d5eadf8bf996e65bcefb69dbfc3f4
kji304 <ingentr75@gmail.com>
2021-07-06 15:48:46 -0400

    Uploaded new files

---
+++ data/products.js
@@ -0,0 +1,76 @@
+const products = [
+    {
+      name: 'Arturo Fuente Cigars',
+      image: '/images/arturo-fuente-cigar.jpg',
+      description:
+        'Arturo Fuente cigars offer a unique blend of flavors',
+      brand: 'Arturo Fuente',
+      category: 'Cigars',
+      price: 89.99,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 12,
+    },
+    {
+      name: 'Davidoff Cigars',
+      image: '/images/davidoff-cigar.jpg',
+      description:
+        'Davidoff cigars offer a unique blend of flavors',
+      brand: 'Davidoff',
+      category: 'Cigars',
+      price: 59.99,
+      countInStock: 7,
+      rating: 4.0,
+      numReviews: 8,
+    },
+    {
+      name: 'Padron Cigar',
+      image: '/images/padron-cigar.jpg',
+      description:
+        'Padron cigars offer a unique blend of flavors',
+      brand: 'Padron',
+      category: 'Cigars',
+      price: 99.99,
+      countInStock: 5,
+      rating: 3,
+      numReviews: 12,
+    },
+    {
+      name: 'Pipe - Cherry',
+      image: '/images/pipe-cherry.jpg',
+      description:
+        'A classic pipe with a cherry finish',
+      brand: 'LHCP',
+      category: 'Pipes',
+      price: 39.99,
+      countInStock: 11,
+      rating: 5,
+      numReviews: 12,
+    },
+    {
+      name: 'Pipe - Light',
+      image: '/images/pipe-light.jpg',
+      description:
+        'A classic pipe with a light finish',
+      brand: 'LHCP',
+      category: 'Pipes',
+      price: 49.99,
+      countInStock: 7,
+      rating: 3.5,
+      numReviews: 10,
+    },
+    {
+      name: 'Spigot Pipe',
+      image: '/images/pipe-spigot.jpg',
+      description:
+        'A spigot style pipe with a dark finish',
+      brand: 'LHCP',
+      category: 'Pipes',
+      price: 29.99,
+      countInStock: 0,
+      rating: 4,
+      numReviews: 12,
+    },
+  ]
+  
+  export default products;
